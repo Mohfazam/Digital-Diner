dc0da5f896ce3a5b05147b57b8b5d4acc0c13383
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-29 00:23:35 +0530

    Signup Completed

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,12 +15,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PgDBURL = process.env.pgUrl;
 const pgClient = new pg_1.Client(PgDBURL);
 pgClient.connect().then(() => { console.log("Connected to the Postgres DB"); });
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, email, password, phoneNumber } = req.body;
+    try {
+        const hashedPassword = yield bcrypt_1.default.hash(password, 10);
+        const result = yield pgClient.query(`INSERT INTO diner_users (username, email, password_hash, phone_number) VALUES ($1, $2, $3, $4) RETURNING id`, [username, email, hashedPassword, phoneNumber]);
+        res.status(200).json({
+            message: "User Signed Up",
+            username, email, phoneNumber
+        });
+    }
+    catch (e) {
+        res.status(400).json({
+            Message: "something is very wrong"
+        });
+    }
+}));
 app.listen(3000, () => {
     console.log("Server Started at the port 3000");
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,33 +16,25 @@ pgClient.connect().then(() => {console.log("Connected to the Postgres DB")});
 
 
 app.post("/signup", async (req, res) => {
-    const { username, email, password, phoneNummber } = req.body;
+    const { username, email, password, phoneNumber } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pgClient.query(
-        `INSERT INTO users (username, email, password_hash, phone_number) VALUES ($1, $2, $3, $4)`
-    )
+        `INSERT INTO diner_users (username, email, password_hash, phone_number) VALUES ($1, $2, $3, $4) RETURNING id`, [ username, email, hashedPassword, phoneNumber]
+    );
+
+    res.status(200).json({
+        message: "User Signed Up",
+        username, email, phoneNumber
+    })
+    } catch(e){
+        res.status(400).json({
+            Message: "something is very wrong"
+        });
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
