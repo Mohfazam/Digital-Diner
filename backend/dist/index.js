3f8dffdb357e5d95de951d319d1b158fab5a7636
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-28 23:55:10 +0530

    Postgres SetUp

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello Nigga");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const pg_1 = require("pg");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const PgDBURL = process.env.pgUrl;
+const pgClient = new pg_1.Client(PgDBURL);
+pgClient.connect().then(() => { console.log("Connected to the Postgres DB"); });
+app.listen(3000, () => {
+    console.log("Server Started at the port 3000");
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello Nigga");
+import express from "express";
+import {Client, Query} from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PgDBURL = process.env.pgUrl;
+
+const pgClient = new Client(PgDBURL);
+
+pgClient.connect().then(() => {console.log("Connected to the Postgres DB")})
+
+app.listen(3000, () => {
+    console.log("Server Started at the port 3000");
+})
